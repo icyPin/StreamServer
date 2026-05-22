1c3a3fd75aaed67229b7a68cd635f60a6d6f425d
Anmol Agarwal <anmolagarwal5159@gmail.com>
2026-05-22 15:14:29 +0530

    restructuring

---
+++ backend-java/src/main/java/com/example/StreamingService/Model/Episode.java
@@ -0,0 +1,15 @@
+package com.example.StreamingService.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Episode {
+
+    private String title;
+    private String videoUrl;
+
+}
---
+++ backend-java/src/main/java/com/example/StreamingService/Model/Show.java
@@ -0,0 +1,18 @@
+package com.example.StreamingService.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Show {
+
+    private String showName;
+    private List<Episode> episodes;
+    private String thumbnailUrl;
+
+}
