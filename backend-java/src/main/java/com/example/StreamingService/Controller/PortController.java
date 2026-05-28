4a2bff08550ed944c88aa57c8024fc6ad098b880
Anmol Agarwal <anmolagarwal5159@gmail.com>
2026-05-29 00:11:51 +0530

    added custom local networking detection

---
+++ backend-java/src/main/java/com/example/StreamingService/Controller/PortController.java
@@ -0,0 +1,19 @@
+//package com.example.StreamingService.Controller;
+//
+//
+//import com.example.StreamingService.Service.NetworkingService;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api/v1/port")
+//public class PortController {
+//
+//    NetworkingService portFindingService;
+//
+//    public PortController(NetworkingService portFindingService){
+//        this.portFindingService=portFindingService;
+//    }
+//
+//    public String
+//}
---
+++ backend-java/src/main/java/com/example/StreamingService/Service/NetworkingService.java
@@ -0,0 +1,42 @@
+package com.example.StreamingService.Service;
+
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Service;
+
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceInfo;
+import java.net.InetAddress;
+
+@Service
+public class NetworkingService {
+
+    private JmDNS jmDNS;
+
+    @PostConstruct
+    public void registerService(){
+        try{
+            jmDNS = JmDNS.create(InetAddress.getLocalHost());
+            ServiceInfo serviceInfo = ServiceInfo.create(
+                    "_http._tcp.local.",
+                    "netflix-gesture",
+                    8086,
+                    "Local Anime Streaming Server"
+            );
+
+            jmDNS.registerService(serviceInfo);
+
+            System.out.println("🌐 Broadcasting on WiFi as: http://netflix-gesture.local:8086");
+
+        } catch (Exception e) {
+            System.out.println("Error networking JmDNS"+e);
+        }
+    }
+
+    @PreDestroy
+    public void unregisterService(){
+        if(jmDNS!=null)
+            jmDNS.unregisterAllServices();
+    }
+}
